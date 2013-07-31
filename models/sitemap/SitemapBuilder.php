6ddd823e2253f4a71b4273e318a07fad494ae4fc
Raymond Julin <raymond.julin@gmail.com>
2013-07-31 09:40:17 +0200

    Dont list non-published content objects, and fixes some php warnings

--- models/sitemap/SitemapBuilder.php
+++ models/sitemap/SitemapBuilder.php
@@ -2,9 +2,6 @@
 namespace sitemap\models\sitemap;
 
 class SitemapBuilder {
-
-
-
     /**
      * @static
      * @param \eZContentObjectTreeNode $node
@@ -13,20 +10,22 @@ class SitemapBuilder {
      */
     public static function buildNodeList(\eZContentObjectTreeNode $node, $excludeCfg = array())
     {
-
         static $excludes;
-
 
         if (!isset($excludes))
             $excludes = $excludeCfg;
 
-        if (!$node instanceof \eZContentObjectTreeNode || !$node->canRead())
-            return null;
+        if (!$node instanceof \eZContentObjectTreeNode)
+            return array();
+        if (!$node->attribute('contentobject_is_published'))
+            return array();
+        if (!$node->canRead())
+            return array();
 
         $classIdentifier = $node->classIdentifier();
         $nodeId = $node->NodeID;
         $sectionId = $node->object()->attribute('section_id');
-#	print $sectionId;
+
         // HANDLE EXCLUDES
         if (isset($excludes['class_identifiers']) && in_array($classIdentifier, $excludes['class_identifiers']))
             return null;
@@ -49,9 +48,7 @@ class SitemapBuilder {
         );
 
         $list[] = $item;
-
-        foreach($node->children() AS $childNode)
-        {
+        foreach ($node->children() as $childNode) {
             $list = array_merge($list, self::buildNodeList($childNode));
         }
 
@@ -65,30 +62,21 @@ class SitemapBuilder {
      */
     public static function xml(array $list)
     {
-
         /* @var $doc DOMDocuemnt */
         $doc = new \DOMDocument('1.0', 'UTF-8');
 
         $urlset = $doc->createElement('urlset');
         $urlset->setAttribute('xmlns', 'http://www.sitemaps.org/schemas/sitemap/0.9');
 
-        foreach($list AS $item)
-        {
+        foreach($list as $item) {
             $url = $doc->createElement('url');
-
-            $loc = $doc->createElement('loc', $item['url']);
-            $url->appendChild($loc);
-
-            $lastmod = $doc->createElement('lastmod', date("c",$item['modified']));
-            $url->appendChild($lastmod);
+            $url->appendChild($doc->createElement('loc', $item['url']));
+            $url->appendChild($doc->createElement('lastmod', date("c",$item['modified'])));
 
             $urlset->appendChild($url);
         }
 
-
-
         $doc->appendChild($urlset);
-
         return $doc;
 
     }
--- modules/sitemap/module.php
+++ modules/sitemap/module.php
@@ -4,10 +4,9 @@
  * @since 2011-02-05
  */
 ezote\Autoloader::register();
-if (isset($Params) && isset($Params['FunctionName']))
-{
+if (isset($Params) && isset($Params['FunctionName'])) {
     $router = new \ezote\lib\Router;
-    $Result = $router->handle('sitemap', 'sitemap', $Params['FunctionName'], $Params['Parameters'])->run();
+    $Result = $router->legacyHandle('sitemap', 'sitemap', $Params['FunctionName'], $Params['Parameters'])->run();
 }
 else
     list($Module, $FunctionList, $ViewList) = sitemap\modules\sitemap\Sitemap::getDefinition();
